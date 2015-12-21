088305cf561f4e50d43ac9f4f5fff66aa53c2f03
Budi Prakosa a.k.a Iyok <iyok@deadmediafm.org>
2015-12-21 10:54:08 +0700

    make binary portable (osx)

--- build_osx.sh
+++ build_osx.sh
@@ -5,7 +5,7 @@ PORT=""
 cwd=$(pwd)
 
 REPO_URL="https://github.com/ATtinyTeenageRiot/AvrdudeGnusbuinoMidi2Kicker.git"
-REPO_DIR="tools/staging/AvrdudeGnusbuinoMidi2Kicker"
+REPO_DIR="staging/AvrdudeGnusbuinoMidi2Kicker"
 DESTINATION_REPO="releases/AvrdudeGnusbuinoMidi2Kicker"
 STAGING_DIR="tools/staging"
 
@@ -31,6 +31,18 @@ pwd
 git log -1 > changelog.txt
 
 mkdir -p "${cwd}/${DESTINATION_REPO}/osx"
+
+rm -fr "${cwd}/${DESTINATION_REPO}/osx/*"
+
 cp ./avrdude "${cwd}/${DESTINATION_REPO}/osx"
 cp ./avrdude.conf "${cwd}/${DESTINATION_REPO}/osx"
 cp ./changelog.txt "${cwd}/${DESTINATION_REPO}/osx"
+
+cp "/usr/local/opt/libusb/lib/libusb-1.0.0.dylib" "${cwd}/${DESTINATION_REPO}/osx"
+
+cd "${cwd}/${DESTINATION_REPO}/osx"
+
+chmod 755 "./libusb-1.0.0.dylib"
+install_name_tool -change  /usr/lib/libedit.3.dylib /usr/lib/libedit.2.dylib ./avrdude
+install_name_tool -change "/usr/local/opt/libusb/lib/libusb-1.0.0.dylib" "@executable_path/libusb-1.0.0.dylib" ./avrdude
+install_name_tool -id "@executable_path/libusb-1.0.0.dylib" ./libusb-1.0.0.dylib
